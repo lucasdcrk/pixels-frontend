1694b2462bb809fb6d217d75843eaffa1927ae93
Lucas <lucasdecrock@outlook.com>
2020-03-24 15:22:26 +0100

    Added some padding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <h1>Welcome to Pixel Wall</h1>
+        <br>
         <router-link to="wall">View the pixel wall</router-link>
-        &nbsp;
+        &nbsp;&nbsp;&nbsp;
         <router-link to="putpixel">Add pixels</router-link>
     </div>
 </template>
